3d76b211bd62662c381e8095df1d6c3092147fce
zcg <1697859639@qq.com>
2022-04-19 21:51:37 +0800

    clap接收参数及生成completions

---
+++ src/cli/clap.rs
@@ -0,0 +1,51 @@
+use std::ffi::OsString;
+use std::io;
+
+use anyhow::{anyhow, Result};
+use clap::{App, Arg, ArgMatches, Shell};
+
+pub struct ClapApplication {
+    pub app: App<'static, 'static>,
+}
+
+pub const BANNER: &str = "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|n|e|t|e|a|s|e|-|c|l|o|u|d|-|m|u|s|i|c|-|t|u|i|
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+";
+use std::io::Write;
+
+impl ClapApplication {
+    pub fn new() -> Self {
+        ClapApplication {
+            app: App::new(env!("CARGO_PKG_NAME"))
+                .version(env!("CARGO_PKG_VERSION"))
+                .author(env!("CARGO_PKG_AUTHORS"))
+                .about(env!("CARGO_PKG_DESCRIPTION"))
+                .usage("Press `?` while running the app to see keybindings")
+                .before_help(BANNER)
+                .after_help("you config path is ...")
+                .arg(
+                    Arg::with_name("completions")
+                        .long("completions")
+                        .help("Generates completions for your preferred shell")
+                        .takes_value(true)
+                        .possible_values(&["bash", "zsh", "fish", "power-shell", "elvish"])
+                        .value_name("SHELL"),
+                ),
+        }
+    }
+
+    pub fn gen_completions(&mut self, completions: &str) -> Result<()> {
+        let shell = match completions {
+            "bash" => Shell::Bash,
+            "zsh" => Shell::Zsh,
+            "fish" => Shell::Fish,
+            "power-shell" => Shell::PowerShell,
+            "elvish" => Shell::Elvish,
+            _ => return Err(anyhow!("no completions available for '{}'", completions)),
+        };
+        self.app
+            .gen_completions_to("cloud-music", shell, &mut io::stdout());
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
 #[macro_use]
 extern crate lazy_static;
 
+use crate::api::ApiEvent;
+use anyhow::Result;
 use ncmapi::NcmApi;
+use std::sync::mpsc;
 
+use crate::cli::clap::ClapApplication;
 use crate::http::request::RequestClient;
 
+mod api;
 mod cli;
 mod http;
 mod ui;
 mod util;
 
-#[tokio::main]
-async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
-    let api = NcmApi::default();
-    let resp = api.cloud_search("mota", None).await;
-    let res = resp.unwrap().deserialize_to_implict();
-    println!("{:#?}", res);
+// #[tokio::main]
+// async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+//     let api = NcmApi::default();
+//     let resp = api.cloud_search("mota", None).await;
+//     let res = resp.unwrap().deserialize_to_implict();
+//     println!("{:#?}", res);
+//
+//     Ok(())
+// }
 
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut clap_app = ClapApplication::new();
+    let matches = clap_app.app.clone().get_matches();
+    if let Some(s) = matches.value_of("completions") {
+        return clap_app.gen_completions(s);
+    }
+
+    let (sync_io_tx, sync_io_rx) = mpsc::channel::<ApiEvent>();
     Ok(())
 }
